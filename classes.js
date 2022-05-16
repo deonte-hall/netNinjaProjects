1e5437667705292d6b64e316a049d52c7e46811b
Deonte H (Sasko) <deonte@Saskos-MacBook-Pro.local>
2022-05-16 14:45:39 -0700

    Added an extended class and a few example methods within. Also a bit more commented code

--- classes.js
+++ classes.js
@@ -1,10 +1,10 @@
 /*
-This is a class in JavaScript
+---This is a class in JavaScript
 
-Classes can be used to create multiple objects of the same type and perform methods using them 
+- Classes can be used to create multiple objects of the same type and perform methods using them 
 
-They Are created using the "class" keyword along with the name of the class and square brackets beneath 
-The "this" keyword represents the current object , and we can store properties for our class using the "this" keyword 
+- They Are created using the "class" keyword along with the name of the class and square brackets beneath 
+- The "this" keyword represents the current object , and we can store properties for our class using the "this" keyword 
 
 
 */
@@ -34,5 +34,29 @@ class Cat {
 // To chain multiple class methods, make sure not to return "undefined" and return the "this" keyword (the current object)
 
 let Mandy = new Cat("black", "large", 102);
+let Billy = new Cat("grey", "small", 30);
+let Garfield = new Cat("orange", "extra large", 120);
 
 console.log(Mandy.purr().checkSize().checkWeight())
+
+/*
+--- This is class inheritance 
+- We don't need a constructor in the extension class, it will use the original classes' constructor by default 
+- The class will have all the same functionality (methods & properties) as the original class 
+- New methods can be created in the class
+*/ 
+
+class SpecialCat extends Cat{
+    alphaCat(cat){
+        let aplhaKitty = arrayToStoreCats.filter(kitty =>{
+            arrayToStoreCats.push(lion);
+            return cat == kitty;
+        })
+        return aplhaKitty
+    }
+
+}
+
+let arrayToStoreCats = [Mandy, Billy, Garfield]
+let lion = new SpecialCat("brown", "huge", 300);
+console.log(Mandy.purr());
